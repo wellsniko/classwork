62cdeded5b79a7d104354d7532cba5bc7bae57e9
Niko Wells <wellsniko@gmail.com>
2020-10-10 14:27:02 -0700

    Poly Tree first go through

---
+++ w3d5/poly_tree_node/lib/00_tree_node.rb
@@ -0,0 +1,39 @@
+class PolyTreeNode
+    attr_accessor :parent, :children, :value
+    def initialize(value)
+        @value = value
+        # @value = value
+        @parent = nil
+        @children = []
+    end
+    
+    def parent=(new_parent)
+       
+        @parent.children.delete(self) if @parent != nil
+        @parent = new_parent
+        @parent.children << self if new_parent != nil && !@parent.children.include?(self) 
+            
+    end
+
+    def add_child(new_child)
+        new_child.parent = self if !self.children.include?(new_child)
+    end
+
+    def remove_child(child)
+        raise "node is not a child" if child.parent == nil
+        child.parent = nil
+    end
+
+    def dfs(target)
+        
+        return self if self.value == target
+        return nil if self.value != target
+        self.children.each do |child|
+            result =  child.dfs(target)
+            if result =! nil
+                return result
+            end
+        end
+        return nil if result != target
+    end
+end
