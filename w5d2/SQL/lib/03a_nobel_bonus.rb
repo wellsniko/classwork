d54fa1da9dbe1189b86a49cc04d35dcbd69d6dbf
Niko Wells <wellsniko@gmail.com>
2020-10-20 17:42:55 -0700

    SQL zoo

---
+++ w5d2/SQL/lib/03a_nobel_bonus.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+  SELECT distinct yr
+  FROM
+    nobels
+  WHERE
+    subject = 'Physics'
+    AND yr NOT IN (SELECT yr FROM nobels WHERE subject = 'Chemistry')
+  SQL
+end
+#[["1917"], ["1919"], ["1924"], ["1933"]]
