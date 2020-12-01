7ca51dbf0a0f8220234c69fb88ef32ce7c95dc9a
Niko Wells <wellsniko@gmail.com>
2020-11-30 18:14:51 -0800

    todos part 2 project

---
+++ w10d3/todos_pt1/frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToDoListContainer from './todos/todo_list_container'
+const App =() => {
+
+    return (
+       <div>  
+            <h1>From the App File inside the root...</h1>
+            <div><ToDoListContainer/></div>
+       </div>
+    )
+}
+
+export default App; 
